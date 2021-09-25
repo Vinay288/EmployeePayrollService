121b3fe5e8656cc8b5ef711f39cca9574cbc90a8
Vinay288 <vinayhiremath288@gmail.com>
2021-09-25 22:18:47 +0530

    ADDED:test to check if file exists

---
+++ src/test/java/NIOFileAPITest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class NIOFileAPITest {
+    private static String HOME = System.getProperty("user.home");
+    private static String PLAY_WITH_NIO = "TempPlayGround";
+
+    @Test
+    public void givenPath_WhenCorrect_CheckIfFileExist() {
+        Path homePath = Paths.get(HOME);
+
+        Assertions.assertTrue(Files.exists(homePath));
+    }
+}
