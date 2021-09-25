6c83a93d7a86ac9b91382d962b23c6b534cb337b
Vinay288 <vinayhiremath288@gmail.com>
2021-09-25 22:28:52 +0530

    ADDED:test to create a directory and check if it exists

--- src/test/java/NIOFileAPITest.java
+++ src/test/java/NIOFileAPITest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,8 +12,25 @@ public class NIOFileAPITest {
 
     @Test
     public void givenPath_WhenCorrect_CheckIfFileExist() {
-        Path homePath = Paths.get(HOME);
-
+        Path homePath = Paths.get("./"+PLAY_WITH_NIO);
         Assertions.assertTrue(Files.exists(homePath));
     }
+    @Test
+    public void givenPath_WhenCorrect_CheckIfFileNotExist() {
+        Path playPath = Paths.get("./"+PLAY_WITH_NIO);
+        if(Files.exists(playPath))
+            FileUtils.deleteFiles(playPath.toFile());
+        Assertions.assertFalse(Files.exists(playPath));
+    }
+    @Test
+    public void createDirectory_WhenCorrect_CheckIfDirectoryExist() {
+        Path playPath = Paths.get(HOME+"/"+PLAY_WITH_NIO);
+        try {
+            Files.createDirectories(playPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertTrue(Files.exists(playPath));
+    }
+
 }
