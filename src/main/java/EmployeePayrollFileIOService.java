2977cbb3517bc7735fee4c53d8c16c2a9e67826c
Vinay288 <vinayhiremath288@gmail.com>
2021-09-25 23:12:44 +0530

    ADDED: function to count entries

--- src/main/java/EmployeePayrollFileIOService.java
+++ src/main/java/EmployeePayrollFileIOService.java
@@ -20,6 +20,7 @@ public class EmployeePayrollFileIOService {
             e.printStackTrace();
         }
     }
+
     public void printData() {
         try {
             Files.lines(new File(PAYROLL_FILE_NAME).toPath()).forEach(System.out::println);
@@ -27,4 +28,15 @@ public class EmployeePayrollFileIOService {
             e.printStackTrace();
         }
     }
+
+    public long countEntries() {
+        long entries = 0;
+        try {
+            entries = Files.lines(new File(PAYROLL_FILE_NAME).toPath()).count();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        return entries;
+    }
+
 }
