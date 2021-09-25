7cbddb397871f03829af79c2a8167b3ae77daaa8
Vinay288 <vinayhiremath288@gmail.com>
2021-09-25 23:24:56 +0530

    ADDED: test to read data from file

--- src/test/java/EmployeePayrollServiceTest.java
+++ src/test/java/EmployeePayrollServiceTest.java
@@ -17,4 +17,8 @@ public class EmployeePayrollServiceTest {
         long entries = employeePayService.countEntries(EmployeePayrollService.IOService.FILE_IO);
         Assertions.assertEquals(3, entries);
     }
+    @Test public void givenFilesOnReadingFromFilesShouldMatchEmployeeCount() {
+        EmployeePayrollService employeePayrollService=new EmployeePayrollService();
+        long entries=employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
+    }
 }
