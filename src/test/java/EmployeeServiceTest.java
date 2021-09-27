5a0b95095e6f4acb4a6300f5f9e59566fe97b64a
Vinay288 <vinayhiremath288@gmail.com>
2021-09-27 19:15:40 +0530

    MODIFIED: test to check for the update

--- src/test/java/EmployeeServiceTest.java
+++ src/test/java/EmployeeServiceTest.java
@@ -2,14 +2,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class EmployeeServiceTest {
     @Test
     public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEntries() {
         Employee[] arrayOfEmployees = {
-                new Employee(1, "Jeff Bezos"),
-                new Employee(2, "Bill Gates"),
-                new Employee(3, "Mark Zuckerberg")
+                new Employee(1, "asdf"),
+                new Employee(2, "qwerty"),
+                new Employee(3, "zxcv")
         };
         EmployeePayrollService employeePayService = new EmployeePayrollService();
         employeePayService = (EmployeePayrollService) Arrays.asList(arrayOfEmployees);
@@ -21,26 +22,25 @@ public class EmployeeServiceTest {
     @Test
     public void givenFilesOnReadingFromFilesShouldMatchEmployeeCount() {
         EmployeePayrollService employeePayrollService = new EmployeePayrollService();
-        long entries = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
+        long entries = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO).size();
     }
 
     @Test
-    public void readFromEmployeePayroll_readAllEmployee() {
+    public void readFromEmployeePayroll_readGivenEmployee() {
         EmployeePayrollService employeePayrollService = new EmployeePayrollService();
-        long size = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO);
-        Assertions.assertEquals(2, size);
+        long size = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO, "vinay").size();
+        Assertions.assertEquals(1, size);
     }
 
     @Test
     public void updateBasicPayInPayroll_CheckIfUpdated() {
-        try {
-            EmployeePayrollService employeePayrollService = new EmployeePayrollService();
-            Payroll actualPayroll = employeePayrollService.updatePayroll();
-            Payroll expectedPayroll = new Payroll(2, 30000000, 600, 500, 250, 7000);
-            Assertions.assertEquals(expectedPayroll.toString(), actualPayroll.toString());
-        } catch (DBException e) {
-            e.printStackTrace();
-        }
+        String name = "terissa";
+        Double basicPay = 300.00;
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<Employee> employeeList = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO, name);
+        employeePayrollService.updatePayroll(name, basicPay);
+        boolean result = employeePayrollService.compareUpdateSync(name);
+        Assertions.assertTrue(result);
     }
 
 }
