571cb7b180dcce9c93bcd97c8dd3160d1c9e78b3
Vinay288 <vinayhiremath288@gmail.com>
2021-09-27 17:49:30 +0530

    MODIFIED: payroll and company to employee objects as attributes constructor

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.util.List;
 
 public class Employee {
     private  Integer companyId;
@@ -8,20 +9,23 @@ public class Employee {
     private LocalDate startDate;
     public int id;
     public String name;
+    private Payroll payroll;
+    private Company company;
 
     public Employee(int id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    public Employee(int id, String name, String gender, String address, Long phoneNumber, LocalDate startDate,Integer companyId) {
+    public Employee(int id, String name, String gender, String address, Long phoneNumber, LocalDate startDate,Payroll payroll,Company company) {
         this.id = id;
         this.name = name;
         this.gender = gender;
         this.address = address;
         this.phoneNumber = phoneNumber;
         this.startDate = startDate;
-        this.companyId=companyId;
+        this.payroll=payroll;
+        this.company=company;
 
     }
 
