dac8cd9f355b3de6d80a91399521eb8f17675a7d
Vinay288 <vinayhiremath288@gmail.com>
2021-09-28 20:47:45 +0530

    ADDED:getters and setters for all attributes

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,8 +1,7 @@
 import java.time.LocalDate;
-import java.util.List;
 
 public class Employee {
-    private  Integer companyId;
+    private Integer companyId;
     private String gender;
     private String address;
     private Long phoneNumber;
@@ -25,6 +24,70 @@ public class Employee {
         this.payroll = payroll;
     }
 
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
     public Employee(int id, String name, String gender, String address, Long phoneNumber, LocalDate startDate, Payroll payroll, Company company) {
         this.id = id;
         this.name = name;
@@ -32,17 +95,19 @@ public class Employee {
         this.address = address;
         this.phoneNumber = phoneNumber;
         this.startDate = startDate;
-        this.payroll=payroll;
-        this.company=company;
+        this.payroll = payroll;
+        this.company = company;
 
     }
-    public Employee(int id, String name, String gender, String address, Long phoneNumber, LocalDate startDate) {
+
+    public Employee(int id, String name, String gender, String address, Long phoneNumber, LocalDate startDate, int companyId) {
         this.id = id;
         this.name = name;
         this.gender = gender;
         this.address = address;
         this.phoneNumber = phoneNumber;
         this.startDate = startDate;
+        this.companyId = companyId;
     }
 
     @Override
